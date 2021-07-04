f1e4e68d0bc32b122231658e09512b28abb0aed8
adigraj30 <82478243+adigraj30@users.noreply.github.com>
2021-07-05 09:45:07 +1200

    MENU
    Watched tut, and got some of my buttons to move to another screen when pressed!!1

---
+++ TitleScreen.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+
+
+func _ready():
+	pass
+	
+	
+func _physics_process(delta):
+	pass
+	
+
+
+
+func _on_TextureButton_pressed():
+	get_tree().change_scene("menuscreen.tscn")
+
+	
---
+++ menuscreen.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_TextureButton_pressed():
+	get_tree().change_scene("DVC_WRLD.tscn")
+	
+
+
+func _on_TextureButton3_pressed():
+	get_tree().change_scene("TitleScreen.tscn")
